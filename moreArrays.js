00367a4a59438359c8b1ee47a9059746c9341f4f
Your Name <you@example.comcd>
2025-12-16 20:00:45 +0600

    more arrays

---
+++ moreArrays.js
@@ -0,0 +1,20 @@
+//concat ( merge Like wedding)
+const boys = ['Rahim', 'Karim'];
+const girls = ['Mina', 'Rina'];
+const wedding = boys.concat(girls);
+console.log(wedding);
+//join ( it's means to turn into text)
+const words = ['I', 'Love', 'JavaScript'];
+const fullWord = words.join(' ');
+console.log(fullWord);
+
+//flat (leveling - Shoman kora)
+const numbers = [1, 2, [2, 3, 4], 6, 0, [5, 7, 4]];
+const flatNumber = numbers.flat();
+console.log(flatNumber);
+
+//example - "Playlist Manager"
+const songs = ["Song A", "Song B", "Song C", "Song D"];
+console.log(songs.indexOf('Song B'));
+console.log(songs.splice(2, 1,));
+console.log(songs.join(' '));
